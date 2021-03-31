19d47592c6cedf1d4554e0adae68435cdbfff05f
Adam Grady <adam.grady@coyotes.usd.edu>
2021-03-31 16:13:01 -0500

    vue init

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <main class="bg-grey-800">
+
+    </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'App'
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
